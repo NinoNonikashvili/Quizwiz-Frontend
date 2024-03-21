123149034b4b03ed0cd65c6d49d5255cdcfd177b
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-03-21 14:59:14 +0400

    feat: filter component with markup and functionality

--- src/components/icons/CheckIcon.vue
+++ src/components/icons/CheckIcon.vue
@@ -1,8 +1,15 @@
+<script>
+export default {
+  props: {
+    color: String
+  }
+}
+</script>
 <template>
   <svg width="12" height="9" viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M10.6668 1.5L4.25016 7.91667L1.3335 5"
-      stroke="#7F56D9"
+      :stroke="color"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
--- src/components/quiz/GeneralFilter.vue
+++ src/components/quiz/GeneralFilter.vue
@@ -161,7 +161,7 @@ export default {
 <template>
   <button
     @click="openFilter"
-    class="flex items-center my-4 mx-4 py-3 px-[0.875rem] border rounded-xl gap-2 relative"
+    class="flex items-center py-3 px-[0.875rem] border rounded-xl gap-2 relative"
     :class="filterCounter === 0 ? 'border-gray-300' : 'border-black'"
   >
     <FilterIcon :color="filterCounter === 0 ? '#667085' : '#000000'" />
@@ -253,7 +253,7 @@ export default {
               class="absolute top-0 left-0 pointer-events-none w-5 h-5 flex justify-center items-center focus:outline-none border border-gray-300 rounded-md bg-white peer-checked:border-[#7F56D9] peer-checked:bg-[#F9F5FF]"
             >
               <div class="hidden group-has-[:checked]:inline-block">
-                <CheckIcon />
+                <CheckIcon color="#7F56D9" />
               </div>
             </div>
           </div>
@@ -273,7 +273,7 @@ export default {
               class="absolute top-0 left-0 pointer-events-none w-5 h-5 flex justify-center items-center focus:outline-none border border-gray-300 rounded-md bg-white peer-checked:border-[#7F56D9] peer-checked:bg-[#F9F5FF]"
             >
               <div class="hidden group-has-[:checked]:inline-block">
-                <CheckIcon />
+                <CheckIcon color="#7F56D9" />
               </div>
             </div>
           </div>
@@ -432,12 +432,12 @@ export default {
       :class="isOptionsEmpty ? 'hidden' : 'block'"
     >
       <button
-        class="font-inter font-semibold text-lg leading-6 rounded-md text-white bg-purple-500 py-18 px-[2.125rem] w-[15.875rem]"
+        class="font-inter font-semibold text-lg leading-6 rounded-[10px] text-white bg-purple-500 py-18 px-[2.125rem] w-[15.875rem]"
       >
         Confirm
       </button>
       <button
-        class="font-inter font-semibold text-lg leading-6 rounded-md text-gray-700 border border-gray-300 bg-white py-18 px-[2.125rem]"
+        class="font-inter font-semibold text-lg leading-6 rounded-[10px] text-gray-700 border border-gray-300 bg-white py-18 px-[2.125rem]"
         @click="reset(closeFilter)"
       >
         Cancel
@@ -445,7 +445,238 @@ export default {
     </div>
   </div>
   <!-- DESKTOP RESOLUTION FILTER  -->
-  <div :class="filterActive ? 'hidden xl:flex ' : 'hidden xl:hidden'">desktop</div>
+  <div
+    class="w-[66.625rem] border border-black rounded-xl p-4 flex-col"
+    :class="filterActive ? 'hidden xl:flex ' : 'hidden xl:hidden'"
+  >
+    <header class="bg-gray-75 rounded-xl p-4 flex gap-4 items-center w-full">
+      <button class="flex items-center py-3 px-[0.875rem] bg-black rounded-xl gap-2">
+        <FilterIcon color="#ffffff" />
+        <span class="font-inter font-normal text-sm leading-6 text-white">Filter</span>
+      </button>
+      <!-- SEARCH -->
+      <div class="flex flex-1 items-center bg-white px-6 py-3 border border-gray-300 rounded-full">
+        <div class="pr-4">
+          <SearchIcon />
+        </div>
+        <input
+          type="text"
+          class="w-full placeholder-gray-500 font-inter text-sm leading-normal font-normal text-gray-500 bg-transparent focus:outline-none"
+          placeholder="Search"
+          @input="searchFilter"
+        />
+      </div>
+      <!-- CONFIRM/CANCEL -->
+      <div class="w-fit flex items-center gap-4" :class="isOptionsEmpty ? 'hidden' : 'block'">
+        <button
+          class="font-inter font-semibold text-sm leading-6 rounded-[10px] text-white bg-purple-500 py-[0.625rem] px-8 w-fit"
+        >
+          Confirm
+        </button>
+        <button
+          class="font-inter font-normal text-sm leading-6 text-gray-500 px-4 border-l border-gray-300"
+          @click="reset(null)"
+        >
+          Reset all filters
+        </button>
+      </div>
+      <div @click="closeFilter">
+        <CrossIcon />
+      </div>
+    </header>
+    <div class="flex gap-[0.625rem]">
+      <!-- FILTER -->
+      <div class="w-[61%] border border-gray-300 rounded-xl p-4">
+        <h3 class="font-inter font-semibold text-sm text-purple-500 mb-4">Filter by</h3>
+        <!-- QUIZES CHECKBOXES -->
+        <div class="flex flex-col gap-4 pb-5 border-b border-gray-300">
+          <div class="flex gap-2 items-center">
+            <label class="font-inter font-semibold text-sm text-gray-900">My Quizes</label>
+            <div class="relative group">
+              <input
+                type="checkbox"
+                name="myQuizes"
+                class="opacity-0 peer w-5 h-5"
+                value="my_quizes"
+                v-model="selectedOptions.quizes"
+              />
+              <div
+                class="absolute top-0 left-0 pointer-events-none w-5 h-5 flex justify-center items-center focus:outline-none border border-gray-300 rounded-md bg-white peer-checked:border-[#000000] peer-checked:bg-[#000000]"
+              >
+                <div class="hidden group-has-[:checked]:inline-block">
+                  <CheckIcon color="#ffffff" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex gap-2 items-center">
+            <label class="font-inter font-semibold text-sm text-gray-900">Not Completed</label>
+            <div class="relative group">
+              <input
+                type="checkbox"
+                name="notCompleted"
+                class="opacity-0 peer w-5 h-5"
+                value="not_completed"
+                v-model="selectedOptions.quizes"
+              />
+              <div
+                class="absolute top-0 left-0 pointer-events-none w-5 h-5 flex justify-center items-center focus:outline-none border border-gray-300 rounded-md bg-white peer-checked:border-[#000000] peer-checked:bg-[#000000]"
+              >
+                <div class="hidden group-has-[:checked]:inline-block">
+                  <CheckIcon color="#ffffff" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- LEVELS -->
+        <div class="py-5 border-b border-gray-300">
+          <h6 class="font-inter font-semibold text-sm text-gray-900 mb-4">Levels</h6>
+          <div class="flex flex-wrap gap-2">
+            <button
+              v-for="(level, index) in levels"
+              :key="index"
+              class="rounded-full font-inter font-medium text-sm py-[0.625rem] px-5"
+              :class="[
+                this.searchKeyword.length !== 0 &&
+                !level.title.toLowerCase().includes(searchKeyword.toLowerCase())
+                  ? 'hidden '
+                  : ' ',
+                this.selectedOptions.levels.includes(level.title)
+                  ? 'text-white ' + level.bg_dark
+                  : level.color + ' ' + level.bg
+              ]"
+              @click="setLevel(level.title)"
+            >
+              {{ level.title }}
+            </button>
+          </div>
+        </div>
+        <!-- CATEGORIES -->
+        <div class="pt-5 pb-[7rem] h-[12rem] overflow-auto">
+          <h6 class="font-inter font-semibold text-sm text-gray-900 mb-4">Categories</h6>
+          <div class="flex flex-wrap gap-2">
+            <button
+              v-for="(cat, index) in categories"
+              :key="index"
+              class="font-inter font-semibold text-sm py-2 px-14 rounded-full"
+              :class="[
+                selectedOptions.categories.includes(cat)
+                  ? 'bg-black text-white'
+                  : 'text-gray-600 bg-white',
+                this.searchKeyword.length !== 0 &&
+                !cat.toLowerCase().includes(searchKeyword.toLowerCase())
+                  ? 'hidden '
+                  : ' '
+              ]"
+              @click="setCategory(cat)"
+            >
+              {{ cat }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- SORTING -->
+      <div class="w-[39%] border border-gray-300 rounded-xl p-4">
+        <h3 class="font-inter font-semibold text-sm text-purple-500 mb-4">Sort by</h3>
+        <!-- ASC/DESC -->
+        <div class="w-[22.5rem] group relative">
+          <div
+            class="flex w-full p-3 rounded-lg gap-4 items-center bg-white group-has-[:checked]:bg-gray-200 pointer-events-none"
+          >
+            <AscIcon />
+            <label class="font-inter font-semibold text-sm leading-6 text-gray-600">A-Z</label>
+            <div class="ml-auto hidden group-has-[:checked]:inline-block">
+              <GreenSelectedIcon />
+            </div>
+          </div>
+          <input
+            type="radio"
+            name="sort_alphabet"
+            value="asc"
+            class="opacity-0 absolute top-0 left-0 w-full h-full p-3"
+            v-model="this.selectedOptions.sort_alphabet"
+          />
+        </div>
+        <div class="w-[22.5rem] group relative">
+          <div
+            class="flex w-full p-3 rounded-lg gap-4 items-center bg-white group-has-[:checked]:bg-gray-200 pointer-events-none"
+          >
+            <DescIcon />
+            <label class="font-inter font-semibold text-sm leading-6 text-gray-600">Z-A</label>
+            <div class="ml-auto hidden group-has-[:checked]:inline-block">
+              <GreenSelectedIcon />
+            </div>
+          </div>
+          <input
+            type="radio"
+            name="sort_alphabet"
+            value="desc"
+            class="opacity-0 absolute top-0 left-0 w-full h-full p-3"
+            v-model="this.selectedOptions.sort_alphabet"
+          />
+        </div>
+        <!-- MOST POPULAR -->
+        <div class="w-[22.5rem] group relative">
+          <div
+            class="flex w-full p-3 rounded-lg gap-4 items-center bg-white group-has-[:checked]:bg-gray-200 pointer-events-none"
+          >
+            <PopularIcon />
+            <label class="font-inter font-semibold text-sm leading-6 text-gray-600"
+              >Most popular</label
+            >
+            <div class="ml-auto hidden group-has-[:checked]:inline-block">
+              <GreenSelectedIcon />
+            </div>
+          </div>
+          <input
+            type="checkbox"
+            name="sort_alphabet"
+            class="opacity-0 absolute top-0 left-0 w-full h-full p-3"
+            v-model="this.selectedOptions.sort_popular"
+          />
+        </div>
+        <!-- DATE-->
+        <div class="w-[22.5rem] group relative">
+          <div
+            class="flex w-full p-3 rounded-lg gap-4 items-center bg-white group-has-[:checked]:bg-gray-200 pointer-events-none"
+          >
+            <NewestIcon />
+            <label class="font-inter font-semibold text-sm leading-6 text-gray-600">Newest</label>
+            <div class="ml-auto hidden group-has-[:checked]:inline-block">
+              <GreenSelectedIcon />
+            </div>
+          </div>
+          <input
+            type="radio"
+            name="sort_date"
+            value="newest"
+            class="opacity-0 absolute top-0 left-0 w-full h-full p-3"
+            v-model="this.selectedOptions.sort_date"
+          />
+        </div>
+        <div class="w-[22.5rem] group relative">
+          <div
+            class="flex w-full p-3 rounded-lg gap-4 items-center bg-white group-has-[:checked]:bg-gray-200 pointer-events-none"
+          >
+            <OldestIcon />
+            <label class="font-inter font-semibold text-sm leading-6 text-gray-600">Oldest</label>
+            <div class="ml-auto hidden group-has-[:checked]:inline-block">
+              <GreenSelectedIcon />
+            </div>
+          </div>
+          <input
+            type="radio"
+            name="sort_date"
+            value="oldest"
+            class="opacity-0 absolute top-0 left-0 w-full h-full p-3"
+            v-model="this.selectedOptions.sort_date"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
