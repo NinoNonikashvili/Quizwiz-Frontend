b254b2df625d6dd2768b424cf5febf6ec73d246e
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-04-14 13:13:18 +0400

    fix: remove redundant if check from service

--- src/services/axios/quizes/loadQuizes.js
+++ src/services/axios/quizes/loadQuizes.js
@@ -1,12 +1,7 @@
 import Axios from '@/services/axios/instance'
 
 export default async function (params) {
-  console.log(params)
-  if (params) {
-    return await Axios.get('/api/quizes', {
-      params: params
-    })
-  }
-
-  return await Axios.get('/api/quizes')
+  return await Axios.get('/api/quizes', {
+    params: params
+  })
 }
